84a169d792d5334d60856459e86f704f0def4e6e
fatima <fxtimaahmed14@gmail.com>
2023-02-21 13:27:11 +0000

    working on core reqs

--- src/core.js
+++ src/core.js
@@ -5,6 +5,17 @@
 // "The cake is still baking!" if there are any remaining minutes left,
 // or "You didn't set a timer!" if no value is provided to the parameter
 
+function timerStatus(minsLeft) {
+  if (!minsLeft) {
+    return "You didn't set a timer!"
+  }
+  if (minsLeft === 0) {
+    return "Phil's cake is ready!"
+  }
+  if (minsLeft > 0) {
+    return 'The cake is still baking!'
+  }
+}
 // 2. To help Phil prepare ahead of time, create a function named estimatePrepTime
 // that accepts two parameters:
 // - an array of ingredients (e.g. ["sugar", "milk", "flour", "eggs"])
@@ -13,6 +24,12 @@
 // number of ingredients provided and the prep time per ingredient.
 // If no prep time per ingredient is provided, the function should assume each ingredient
 // takes 2 minutes to prepare
+
+function estimatePrepTime(ingredients, prepTime) {
+  let total = 0
+  total = prepTime * ingredients.length - 1
+  return total
+}
 
 // 3. Phil needs to know the quantity of milk and eggs to use! Create a function
 // named calculateQuantities which accepts two parameters:
@@ -29,6 +46,7 @@
 //
 // calculateQuantities(["milk", "eggs"], 3)
 // returns: { sugar: 0, eggs: 6 }
+function calculateQuantities(ingredients, layers) {}
 
 // 4. Phil's cake is pretty small and only provides 1 portion. He wants to make a bigger one!
 // Create a function named improveRecipe that accepts two parameters:
